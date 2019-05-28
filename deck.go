3b4805cb9fb06ec590c2ad7ae0b1b6574a4a49b4
Danny Rangel <danielrangel.tech@gmail.com>
2019-05-28 14:39:34 -0700

    added shuffle function

--- deck.go
+++ deck.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 type deck []string
@@ -46,6 +48,17 @@ func newDeckFromFile(filename string) deck {
 	}
 
 	return deck(strings.Split(string(bs), " , "))
+}
+
+func (d deck) shuffle() {
+
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPos := r.Intn(len(d) - 1)
+		d[i], d[newPos] = d[newPos], d[i]
+	}
 }
 
 func (d deck) print() {
--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	cards := newDeckFromFile("my_cards")
 
+	// cards.print()
+
+	fmt.Println("After shuffle ---")
+
+	cards.shuffle()
 	cards.print()
 }
